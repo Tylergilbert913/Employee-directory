992f125ebe904790f6d3b82949b71b3226afa7af
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-05-01 01:09:58 -0400

    added styling to the directory header

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import EmployeesContainer from './Components/Container/EmployeesContainer';
+import Header from './Components/Header';
+import EmployeesContainer from '../src/Components/Container/EmployeesContainer';
 
 
 function App() {
