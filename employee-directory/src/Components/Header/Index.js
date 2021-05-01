2cfcc118d152cbae0266e1a3255ee4430659c5cd
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-04-30 23:55:51 -0400

    created the index.js file for SearchBAr

---
+++ employee-directory/src/Components/Header/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+    return (
+        <header>
+            <h1 className="text-center">Employee Directory</h1>
+            <p className="text-center">
+                Click on column headers to filter by heading or use the search box to narrow your results.
+            </p>
+        </header>
+    );
+};
+
+export default Header;
